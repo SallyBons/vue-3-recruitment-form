d78694264dea971e008f2942702c46448e623ea5
SallyBons <46354110+SallyBons@users.noreply.github.com>
2021-02-09 15:04:45 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { VuelidatePlugin } from '@vuelidate/core';
-
+import { VuelidatePlugin } from "@vuelidate/core";
 
 createApp(App)
   .use(store)
